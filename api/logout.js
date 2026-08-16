56ce417e9b60ea94ae25fa61aca08459b73246b1
farbeshasan26-cmyk <farbeshasan26@gmail.com>
2026-08-16 17:28:03 +0600

    Create logout.js

---
+++ api/logout.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
+  }
+
+  res.setHeader(
+    "Set-Cookie",
+    "session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0"
+  );
+
+  return res.status(200).json({
+    success: true
+  });
+}
